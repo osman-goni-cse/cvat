404621e4d00b2e3fdca76d13e455abe06e0bd57a
Osman Goni <osmancse32@gmail.com>
2025-01-29 19:13:36 +0600

    csrf error fix in production

---
+++ local-settings.py
@@ -0,0 +1,7 @@
+# Overlaying production
+from cvat.settings.production import *
+UI_SCHEME = os.environ.get('UI_SCHEME', 'http')
+UI_HOST = os.environ.get('UI_HOST', 'localhost')
+UI_PORT = os.environ.get('UI_PORT', 3000)
+UI_URL = '{}://{}'.format(UI_SCHEME, UI_HOST)
+CSRF_TRUSTED_ORIGINS = [UI_URL]
