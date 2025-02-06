de3e7ca3f26b90039dd5a13f42a78fcec95c7419
Osman Goni <osmancse32@gmail.com>
2025-02-06 11:02:06 +0600

    format code to resolve check failing

--- local-settings.py
+++ local-settings.py
@@ -1,6 +1,7 @@
 # Overlaying production
 from cvat.settings.production import *
-UI_SCHEME = os.environ.get('UI_SCHEME', 'http')
-UI_HOST = os.environ.get('UI_HOST', 'localhost')
-UI_URL = '{}://{}'.format(UI_SCHEME, UI_HOST)
+
+UI_SCHEME = os.environ.get("UI_SCHEME", "http")
+UI_HOST = os.environ.get("UI_HOST", "localhost")
+UI_URL = "{}://{}".format(UI_SCHEME, UI_HOST)
 CSRF_TRUSTED_ORIGINS = [UI_URL]
